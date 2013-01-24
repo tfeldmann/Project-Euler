d452a0b71779c62eb9078cd77734ed0276d8622d
Thomas Feldmann <feldmann.thomas@googlemail.com>
2013-01-24 23:50:22 +0100

    solved #9

--- 9 - Special Pythagorean triplet.py
+++ 9 - Special Pythagorean triplet.py
@@ -6,3 +6,17 @@
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
 
+def is_pythagorean_triplet(a, b, c):
+    return a**2 + b**2 == c**2
+
+def find_pythagorean_triplet_with_sum(n):
+    RANGE = range(1, n)
+    for h in RANGE:
+        for j in RANGE:
+            for k in RANGE:
+                if h + j + k == n and is_pythagorean_triplet(h, j, k):
+                    return [h, j, k]
+
+triplet = find_pythagorean_triplet_with_sum(1000)
+
+print triplet[0] * triplet[1] * triplet[2]  # prints 31875000
